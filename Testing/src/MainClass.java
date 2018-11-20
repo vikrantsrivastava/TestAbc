ac318df1855d53dc566707c1112e6ca85a926509
vikrantkumar <vikrantkumar@vikrants-air.lan>
2018-11-20 15:57:09 +0530

    FRED-02

--- Testing/src/MainClass.java
+++ Testing/src/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello property : " 
-                + System.getProperty("Hello", "Vikrant")); 
+                + System.getProperty("Hello", "Vikrant256")); 
 		
 		System.out.println(new Date());
 		
